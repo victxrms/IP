1e95a52621804f4924cd49ec1c95b208c5601ad7
Victor <vmoso2002@gmail.com>
2022-01-31 19:11:22 +0100

    inacabado

---
+++ ejercicios de examenes/sep21/ej6.c
@@ -0,0 +1,24 @@
+//EJERCICIO 6: Mediante el uso de funciones, hacer los siguientes apartados: 
+
+//a) Crea una función que defina una matriz de enteros y la rellene 
+//b) En otra función calcula la media de los números en la matriz que no estén en la diagonal. 
+//c) En otra función escribe por pantalla el número de números que estén dentro de la matriz y sean 
+//superior al de la media. 
+//d) En otra función crea un vector dinámico que guarde todos los números que estén dentro de la matriz 
+//y sea superior a la media. 
+//e) En otra función crea un fichero binario que escriba el vector.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+
+void media (int mat[][N]);
+void num_media (int mat[][N]);
+void vec_media (int mat[][N]);
+void vec_fich (int mat[][N]);
+
+int main ()
+{
+    
+}
